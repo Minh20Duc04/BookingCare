c29b309068382bc2c7777544afe03a51f5f0026e
Minh20Duc04 <nguyenminhduc02042004@gmail.com>
2025-12-01 15:57:42 +0700

    Patient and Medical Record models

---
+++ src/main/java/com/example/demo/Model/MedicalRecord.java
@@ -0,0 +1,32 @@
+package com.example.demo.Model;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+@Table(name = "medical_record")
+
+public class MedicalRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String diagnosis;
+
+    private String treatmentPlan;
+
+    private String prescription;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
